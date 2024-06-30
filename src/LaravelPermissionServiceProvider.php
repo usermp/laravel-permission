bc877597333c6cb8cd0f6e1c6ccf23623e587d85
mohammad <usermp76@gmail.com>
2024-06-30 15:14:43 +0330

    Update class name for service provider

--- src/LaravelPermissionServiceProvider.php
+++ src/LaravelPermissionServiceProvider.php
@@ -5,7 +5,7 @@ namespace Usermp\LaravelPermission;
 use Illuminate\Support\ServiceProvider;
 use Usermp\LaravelPermission\Services\PermissionService;
 
-class LaravelServiceProvider extends ServiceProvider
+class LaravelPermissionServiceProvider extends ServiceProvider
 {
     public function register()
     {
@@ -19,7 +19,7 @@ class LaravelServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             if (! class_exists('CreatePermissionsTables')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_permissions_tables.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_permissions_tables.php'),
+                    __DIR__ . '/database/migrations/create_permissions_tables.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_permissions_tables.php'),
                 ], 'migrations');
             }
         }
