7a5749fb13187eb6f38e5985860c7a5f6a83da80
mohammad <usermp76@gmail.com>
2024-07-08 14:43:50 +0330

    add

--- src/Commands/GenerateSuperAdmin.php
+++ src/Commands/GenerateSuperAdmin.php
@@ -4,6 +4,7 @@ namespace Usermp\LaravelPermission\Commands;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Console\Command;
+use Usermp\LaravelPermission\Models\ExtendedUser;
 use Usermp\LaravelPermission\Models\Role;
 use Usermp\LaravelPermission\Models\Permission;
 use Usermp\LaravelPermission\Services\PermissionService;
@@ -23,8 +24,16 @@ class GenerateSuperAdmin extends Command
 
     public function handle()
     {
-        $permission = Permission::firstOrCreate(['name' => 'Super Admin']);
+        $userId = $this->argument('UserId');
 
+        // Check if the user exists
+        $user = ExtendedUser::find($userId);
+        if (!$user) {
+            $this->error("User with ID {$userId} does not exist.");
+            return;
+        }
+        $permission = Permission::firstOrCreate(['name' => 'Super Admin']);
+        $user->permissions()->attach($permission->id);
         foreach (Role::all() as $role) {
             $role->permissions()->attach($permission->id, [
                 'created_at' => Carbon::now(),
@@ -32,6 +41,6 @@ class GenerateSuperAdmin extends Command
             ]);
         }
 
-        $this->info('Super Admin permission assigned to all roles successfully.');
+        $this->info('Super Admin permission assigned to user and all roles successfully.');
     }
 }
